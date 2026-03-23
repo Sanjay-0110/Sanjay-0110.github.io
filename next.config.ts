a662f14f91abc4887d47f11d6bb773c927df29ac
Sanjay-0110 <Sjai58066@gmail.com>
2026-03-23 22:16:25 +0000

    changed nextConfig file and deploy.yml file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/sanjay0110.github.io",
   images: {
     unoptimized: true,
   },
